c4c9c916a1880bb4cc0fa587bb8e4bc2a059c5e5
Ahmad Reza Adrian <ahmadadrian324@gmail.com>
2025-02-19 12:18:21 +0700

    feat: create dynamic web scraping functionality #1
    Merge pull request #1 from rezadrian01/dev-scrape-script

---
+++ scrape.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
+
+def scrape(url, timeout = 10):
+  # Headless browser
+  options = webdriver.ChromeOptions()
+  options.add_argument('--headless')
+
+  driver = webdriver.Chrome()
+  driver.get(url)
+  try:
+    # Wait until chat elements has been loaded
+    WebDriverWait(driver, timeout).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-message-author-role="user"]'))
+    )
+    print("Page loaded")
+  except:
+    print("Timeout")
+  
+  html = driver.page_source
+  soup = BeautifulSoup(html, 'html.parser')
+
+  # Get all chat elements with user and assistant roles
+  users_chat_elements = soup.find_all('div', attrs={'data-message-author-role': 'user'})
+  assistant_chat_elements = soup.find_all('div', attrs={'data-message-author-role': 'assistant'})
+
+  # Close the browser
+  driver.quit()
+
+  user_chat = []
+  assistant_chat = []
+
+  for chat in users_chat_elements:
+    user_chat.append(chat.get_text())
+  
+  for chat in assistant_chat_elements:
+    assistant_chat.append(chat.get_text())
+  
+  return user_chat, assistant_chat
+
+url = "https://chatgpt.com/share/67b560a2-b364-8010-83ce-fb50f8ce0151"
+
+user_chat, assitant_chat = scrape(url)
+
+print(f"User chat: (total: {len(user_chat)})")
+for chat in user_chat:
+  print(chat)
+
+print(f"Assistant chat: (total: {len(assitant_chat)})")
+for chat in assitant_chat:
+  print(chat)
+
