76112018273c2a932d9b4062d13127b048d231aa
rezadrian01 <ahmadadrian324@gmail.com>
2025-03-03 16:01:59 +0700

    feat: create authentication feature and change to use SQL Alchemy ORM

--- src/config.py
+++ src/config.py
@@ -5,5 +5,6 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", ""),
-    "database": os.getenv("DB_NAME", "gptarchive"),
+    "database": os.getenv("DB_NAME", "gptarchive-2"),
+    "port": os.getenv("DB_PORT", "3306")
 }
